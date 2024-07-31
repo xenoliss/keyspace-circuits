d3d25e1453fd3796f678fcd47c416206d9443a47
Baptiste Oueriagli <baptiste.oueriagli@coinbase.com>
2024-07-31 20:23:44 +0000

    wip

--- k_lib/src/batcher/tx.rs
+++ k_lib/src/batcher/tx.rs
@@ -12,35 +12,14 @@ pub struct Tx {
 
 impl Tx {
     pub fn apply(&self, root: [u8; 32]) -> [u8; 32] {
-        // keyspace_id = hash(hash(original_vk), hash(original_data))
-        // new_key = hash(hash(new_vk), hash(new_data))
-        //
-        // Insertion:
-        //   node_key = imt_mutate.node.key
-        //
-        //   v_key = record_proof.v_key
-        //   current_data = record_proof.pub_inputs
-        //   hash(hash(v_key), hash(current_data)) == node_key
-        //
-        // Update:
-        //   value_hash = imt_mutate.node.value_hash
-        //
-        //   v_key = record_proof.v_key
-        //   current_data = record_proof.pub_inputs
-        //   hash(hash(v_key), hash(current_data)) == value_hash
-        let keyspace_key = match &self.imt_mutate {
-            IMTMutate::Insert(insert) => insert.node.key,
-            IMTMutate::Update(update) => update.node.value_hash,
-        };
+        // TODO: Verify tx hash.
 
         // If the record proof does not match with the IMTMutate, do not apply the IMTMutate.
-        if keyspace_key != self.record_proof.keyspace_key() {
+        if !self.imt_mutate.is_bound_to_proof(&self.record_proof) {
             return root;
         }
 
         // Apply the IMTMutate and returned the new root.
         self.imt_mutate.apply(root)
-
-        // TODO: Verify tx hash.
     }
 }
